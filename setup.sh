d4c2294cf70966dba94519c508b84372a4e35ccb
Henrik Laun <software@web-hl.de>
2024-12-13 21:00:22 +0100

    fix: setup script fix

--- setup.sh
+++ setup.sh
@@ -1,14 +1,32 @@
+repoPath="$1"
+
+if [ -d "$repoPath" ]; then
+  echo "$repoPath does exist."
+fi
+
+
 sudo apt install alacritty light sway swaylock waybar wofi fonts-font-awesome pulseaudio pavucontrol
 
 mkdir -p ~/.config/alacritty ~/.config/sway ~/.config/swaylock ~/.config/waybar ~/.config/wlogout ~/.config/wofi 
 
 cd ~/.config/sway/
-ln -s ./sway/config . 
+ln -s $repoPath/sway/config . 
+
+cd ~/.config/swaylock/
+ln -s $repoPath/swaylock/config .
+
+cd ~/.config/swaybar/
+ln -s $repoPath/waybar/config .
+ln -s $repoPath/waybar/style.css
+
+cd ~/.config/wlogout/
+ln -s $repoPath/wlogout/layout
+ln -s $repoPath/wlogout/style.css
 
 cd ~/.config/wofi/
-ln -s ./wofi/config .
-ln -s ./wofi/style.css .
+ln -s $repoPath/wofi/config .
+ln -s $repoPath/wofi/style.css .
 
 cd ~/.config/alacritty/
-ls -s ./alacritty/alacritty.yml .
-
+ls -s $repoPath/alacritty/alacritty.yml .
+ 
