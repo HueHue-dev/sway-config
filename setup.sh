144ce06cd6532de661f8418e7ac1435c4bcb672d
Henrik Laun <hlaun@dev-specialists.com>
2024-12-16 16:59:26 +0100

    refactor: changed repo structure

--- setup.sh
+++ setup.sh
@@ -11,23 +11,22 @@ sudo apt install alacritty light sway swaylock waybar wofi fonts-font-awesome pu
 mkdir -p ~/.config/alacritty ~/.config/sway ~/.config/swaylock ~/.config/waybar ~/.config/wlogout ~/.config/wofi 
 
 cd ~/.config/sway/
-ln -s $repoPath/sway/config . 
+ln -s $repoPath/.config/sway/config . 
 
 cd ~/.config/swaylock/
-ln -s $repoPath/swaylock/config .
+ln -s $repoPath/.config/swaylock/config .
 
 cd ~/.config/waybar/
-ln -s $repoPath/waybar/config .
-ln -s $repoPath/waybar/style.css
+ln -s $repoPath/.config/waybar/config .
+ln -s $repoPath/.config/waybar/style.css .
 
 cd ~/.config/wlogout/
-ln -s $repoPath/wlogout/layout .
-ln -s $repoPath/wlogout/style.css .
+ln -s $repoPath/.config/wlogout/layout .
+ln -s $repoPath/.config/wlogout/style.css .
 
 cd ~/.config/wofi/
-ln -s $repoPath/wofi/config .
-ln -s $repoPath/wofi/style.css .
+ln -s $repoPath/.config/wofi/config .
+ln -s $repoPath/.config/wofi/style.css .
 
 cd ~/.config/alacritty/
-ln -s $repoPath/alacritty/alacritty.yml .
- 
+ln -s $repoPath/.config/alacritty/alacritty.yml .
