de2361899ff2e6b89fd7be86a4b1cf947a4d1abd
Henrik Laun <software@web-hl.de>
2024-12-21 04:52:45 +0100

    added notification center package

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ ! -d "$repoPath" ]; then
 fi
 
 
-sudo apt install alacritty light sway swaylock waybar wofi fonts-font-awesome pulseaudio pavucontrol xdg-desktop-portal-wlr flameshot
+sudo apt install alacritty light sway swaylock waybar wofi fonts-font-awesome pulseaudio pavucontrol xdg-desktop-portal-wlr flameshot sway-notification-center
 
 mkdir -p ~/.config/alacritty ~/.config/sway ~/.config/swaylock ~/.config/waybar ~/.config/wlogout ~/.config/wofi ~/.config/xdg-desktop-portal/ ~/.config/swaync/
 
